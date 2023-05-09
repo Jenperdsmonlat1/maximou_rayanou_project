ce6cf4a2ab5823f7a524b1f525c28a413d62eb5f
Jenperdsmonlat1 <65119790+Jenperdsmonlat1@users.noreply.github.com>
2023-05-09 15:02:28 +0200

    Update sketch_may09c.ino
    Adding new feature to control the ESC.

--- sketch_may09c/sketch_may09c.ino
+++ sketch_may09c/sketch_may09c.ino
@@ -6,6 +6,9 @@ void loop() {
   valeur = analogRead(A3);
   valeur = valeur / 1023;
   digitalWrite(4,HIGH);
+  analogWrite(A5,1023);
   delayMicroseconds(1000+valeur*1000);
   digitalWrite(4,LOW);
-  delay(19-valeur);}
+  analogWrite(A5,0);
+  delay(19-valeur);
+}
